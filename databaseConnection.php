ea9c7a1d3426ad533b3cef73a837a43b62ac771a
bveraaSaturnS <63393560+bveraaSaturnS@users.noreply.github.com>
2022-12-14 19:50:47 +0100

    Update databaseConnection.php

--- databaseConnection.php
+++ databaseConnection.php
@@ -4,6 +4,32 @@ $database = "storage";
 $username = "tech";
 $password = "tech";
 
+//style
+body{
+	background-image: url("background.jpg");
+  background-repeat: no-repeat;
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	background-size: 200% ;
+	background-size: 200%;
+	background-position: center;
+  font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+  font-size: 14px;
+  text-align: center;
+  right: -20%;
+
+	font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+	font-size: 14px;
+	text-align: center;
+	right: -20%;
+	height: 100vh;
+  overflow: hidden;
+
+	overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+
 // Create connection
 
 $conn = mysqli_connect($servername, $username, $password, $database);
